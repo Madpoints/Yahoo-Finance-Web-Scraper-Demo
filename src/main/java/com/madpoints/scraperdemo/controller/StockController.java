a609282ce5f8abccfa6a7d89bb6276d36304db72
John Furtado <jf8846@gmail.com>
2018-10-08 19:55:48 -0400

    added spring support

--- src/main/java/com/madpoints/scraperdemo/controller/StockController.java
+++ src/main/java/com/madpoints/scraperdemo/controller/StockController.java
@@ -1,5 +1,16 @@
 package com.madpoints.scraperdemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class StockController {
+	
+	@RequestMapping("/stocks")
+	public String listStocks(Model theModel) {
+		
+		return "stocks";
+	}
 
 }
---
+++ src/main/webapp/WEB-INF/view/stocks.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Stocks
+</title>
+</head>
+<body>
+
+<h2>Web Scraper Demo - Home Page</h2>
+
+<hr>
+
+</body>
+
+</html>
