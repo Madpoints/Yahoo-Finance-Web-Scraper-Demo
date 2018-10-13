f942ea2e256288332018eccf6e681ae8ed0a7b05
John Furtado <jf8846@gmail.com>
2018-10-13 19:54:55 -0400

    auto wire stock service

--- src/main/java/com/madpoints/scraperdemo/controller/StockController.java
+++ src/main/java/com/madpoints/scraperdemo/controller/StockController.java
@@ -7,14 +7,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.madpoints.scraperdemo.entity.Stock;
+import com.madpoints.scraperdemo.service.StockService;
 
 @Controller
 public class StockController {
+	
+	@Autowired
+	private StockService stockService;
 	
 	@RequestMapping("/stocks")
 	public String listStocks(Model theModel) {
