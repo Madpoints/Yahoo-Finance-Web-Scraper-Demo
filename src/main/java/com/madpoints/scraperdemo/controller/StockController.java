cc7377ec58aee8b058fab813ff9c512abc831ec3
John Furtado <jf8846@gmail.com>
2018-10-13 20:35:14 -0400

    moved scraper code to stock service, refactored

--- src/main/java/com/madpoints/scraperdemo/controller/StockController.java
+++ src/main/java/com/madpoints/scraperdemo/controller/StockController.java
@@ -1,12 +1,7 @@
 package com.madpoints.scraperdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,90 +19,11 @@ public class StockController {
 	@RequestMapping("/stocks")
 	public String listStocks(Model theModel) {
 		
-		// path to chrome webdriver exe
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\John\\careerDevs\\Selenium\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.finance.yahoo.com/most-active");
-		
-//		WebElement stockTableElement = driver.findElement(By.xpath("//table[@data-reactid='73']"));
-//		List<Stock> stocks = setWebElementToStock(stockTableElement);
-//		
-//		for (Stock stock : stocks) {
-//			System.out.println(stock.toString());
-//		}
-		
-		List<Stock> stocks = setStocks(driver);
+		List<Stock> stocks = stockService.getStocks();
 		
 		theModel.addAttribute("stocks", stocks);
 		
 		return "list-stocks";
 	}
 	
-	public static List<Stock> setWebElementToStock(WebElement webElement) {
-		
-		List<Stock> stocks = new ArrayList<Stock>();
-		
-		setStockSymbol(stocks, webElement);
-		
-		return stocks;
-	}
-	
-	public static void setStockSymbol(List<Stock> stocks, WebElement webElement) {
-		
-		List<WebElement> stockSymbolElements = webElement.findElements(By.xpath("//td[@aria-label='Symbol']"));
-		
-		for (WebElement stockSymbol : stockSymbolElements) {
-			
-			Stock tempStock = new Stock();
-			tempStock.setSymbol(stockSymbol.getText());
-			stocks.add(tempStock);
-		}
-	}
-	
-	public static List<Stock> setStocks(WebDriver driver) {
-		
-		List<WebElement> stockSymbolElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Symbol']"));
-		List<WebElement> stockNameElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Name']"));
-		List<WebElement> stockPriceElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Price (Intraday)']"));
-		
-		List<Stock> stocks = new ArrayList<Stock>();
-		
-		int size = stockSymbolElements.size();
-		int listIndex = 0;
-		
-		for (int index = 0; index < size; index++) {
-			
-			Stock tempStock = new Stock();
-			stocks.add(index, tempStock);
-			
-		}
-		
-		for (WebElement stockSymbolElement : stockSymbolElements) {
-			
-			stocks.get(listIndex).setSymbol(stockSymbolElement.getText());
-			listIndex++;
-			
-		}
-		
-		listIndex = 0;
-		
-		for (WebElement stockNameElement : stockNameElements) {
-			
-			stocks.get(listIndex).setName(stockNameElement.getText());
-			listIndex++;
-			
-		}
-		
-		listIndex = 0;
-		
-		for (WebElement stockPriceElement : stockPriceElements) {
-			
-			stocks.get(listIndex).setPrice(stockPriceElement.getText());
-			listIndex++;
-			
-		}
-		
-		return stocks;
-	}
-
 }
--- src/main/java/com/madpoints/scraperdemo/service/StockService.java
+++ src/main/java/com/madpoints/scraperdemo/service/StockService.java
@@ -1,5 +1,75 @@
 package com.madpoints.scraperdemo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.stereotype.Service;
+
+import com.madpoints.scraperdemo.entity.Stock;
+
+@Service
 public class StockService {
+	
+	public List<Stock> getStocks() {
+		
+		// path to chrome webdriver exe
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\John\\careerDevs\\Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.finance.yahoo.com/most-active");
+		
+		List<Stock> stocks = setStocks(driver);
+		
+		return stocks;
+	}
+	
+	private static List<Stock> setStocks(WebDriver driver) {
+		
+		List<WebElement> stockSymbolElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Symbol']"));
+		List<WebElement> stockNameElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Name']"));
+		List<WebElement> stockPriceElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Price (Intraday)']"));
+		
+		List<Stock> stocks = new ArrayList<Stock>();
+		
+		int size = stockSymbolElements.size();
+		int listIndex = 0;
+		
+		for (int index = 0; index < size; index++) {
+			
+			Stock tempStock = new Stock();
+			stocks.add(index, tempStock);
+			
+		}
+		
+		for (WebElement stockSymbolElement : stockSymbolElements) {
+			
+			stocks.get(listIndex).setSymbol(stockSymbolElement.getText());
+			listIndex++;
+			
+		}
+		
+		listIndex = 0;
+		
+		for (WebElement stockNameElement : stockNameElements) {
+			
+			stocks.get(listIndex).setName(stockNameElement.getText());
+			listIndex++;
+			
+		}
+		
+		listIndex = 0;
+		
+		for (WebElement stockPriceElement : stockPriceElements) {
+			
+			stocks.get(listIndex).setPrice(stockPriceElement.getText());
+			listIndex++;
+			
+		}
+		
+		return stocks;
+	}
 
 }
