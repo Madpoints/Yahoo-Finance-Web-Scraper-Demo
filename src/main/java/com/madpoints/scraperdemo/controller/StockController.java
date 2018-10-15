1687ddb25c53fab2199c23f71980c8af303a3044
John Furtado <jf8846@gmail.com>
2018-10-15 17:34:07 -0400

    added new url paths

--- src/main/java/com/madpoints/scraperdemo/controller/StockController.java
+++ src/main/java/com/madpoints/scraperdemo/controller/StockController.java
@@ -5,18 +5,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.madpoints.scraperdemo.entity.Stock;
 import com.madpoints.scraperdemo.service.StockService;
 
 @Controller
+@RequestMapping("/stocks")
 public class StockController {
 	
 	@Autowired
 	private StockService stockService;
 	
-	@RequestMapping("/stocks")
+	@GetMapping("/list")
 	public String listStocks(Model theModel) {
 		
 		List<Stock> stocks = stockService.getStocks();
