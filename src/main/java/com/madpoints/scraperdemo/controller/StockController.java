dccf7f41a1eef06b04d656207ae7bc2610cfd8a0
John Furtado <jf8846@gmail.com>
2018-10-16 17:41:40 -0400

    stock info get mapping controller

--- src/main/java/com/madpoints/scraperdemo/controller/StockController.java
+++ src/main/java/com/madpoints/scraperdemo/controller/StockController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.madpoints.scraperdemo.entity.Stock;
 import com.madpoints.scraperdemo.service.StockService;
@@ -26,6 +27,14 @@ public class StockController {
 		theModel.addAttribute("stocks", stocks);
 		
 		return "list-stocks";
+	}
+	
+	@GetMapping("/info")
+	public String showInfo(@RequestParam("stock") String stockInfo, Model theModel) {
+		
+		theModel.addAttribute("stock", stockInfo);
+		
+		return "stock-info";
 	}
 	
 }
